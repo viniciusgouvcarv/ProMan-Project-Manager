eedfe046a4900efbf0fa5cdeeffc3418a9c20097
Cosmopato <44640453+viniciusgouvcarv@users.noreply.github.com>
2020-03-11 20:16:52 -0300

    Trocando banco de dados

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ options = {
 setup(
     name = "ProMan - Project Manager",
     options = options,
-    version = "0.0.3",
+    version = "0.0.4",
     description = 'O seu Gerenciador de Projetos favorito!',
     executables = executables
 )
